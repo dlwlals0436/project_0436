30243895be62f0b729afc7614539f6d694edc359
dlwlals0436 <94441549+dlwlals0436@users.noreply.github.com>
2021-12-27 21:14:49 -0800

    Add files via upload

---
+++ toki0436.py
@@ -0,0 +1,37 @@
+
+
+import requests
+
+import time
+
+from bs4 import BeautifulSoup
+
+import telegram
+
+
+
+bot = telegram.Bot(token='5047253923:AAGRE6Knv20XImmA_OO-WO5x8xW9dSGD2iU')
+
+
+
+if __name__ == '__main__':
+
+    # 제일 최신 게시글의 번호 저장
+    latest_num = 0
+    while True:
+        req = requests.get('https://newtoki118.com/toki_free?sca=%EA%B3%B5%EC%9C%A0')
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        posts = soup.find("li", {"class" : "list-item"})
+        post_num = posts.find("div", {"class" : "wr-num hidden-xs"}).text
+
+
+
+        if latest_num != post_num :
+            latest_num = post_num
+            name =  posts.find("span", {"class" : "member"}).text
+            ago =  posts.find("div", {"class" : "wr-date hidden-xs"}).text
+            text = name + '의 새 글이 올라왔어욤' + '\n' +ago
+            bot.sendMessage(-1001555428405, text)
+        time.sleep(30)
+        print('bot 동작 중 현재 게시글 번호' + latest_num)
