54f646f1d9889e39478375d4a0e24b81b2fd980f
dlwlals0436 <94441549+dlwlals0436@users.noreply.github.com>
2021-12-28 02:16:28 -0800

    Update toki0436.py

--- toki0436.py
+++ toki0436.py
@@ -8,30 +8,23 @@ from bs4 import BeautifulSoup
 
 import telegram
 
-
-
-bot = telegram.Bot(token='5047253923:AAGRE6Knv20XImmA_OO-WO5x8xW9dSGD2iU')
-
-
+bot = telegram.Bot(token='5047253923:AAGRㅁE6Knv20XImmA_OO-WO5x8xW9dSGD2iU')
 
 if __name__ == '__main__':
-
 
     latest_num = 0
     while True:
         req = requests.get('https://newtoki118.com/toki_free?sca=%EA%B3%B5%EC%9C%A0')
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
-        posts = soup.find("li", {"class" : "list-item"})
-        post_num = int(posts.find("div", {"class" : "wr-num hidden-xs"}).text)
+        posts = soup.find("li", {"class": "list-item"})
+        post_num = int(posts.find("div", {"class": "wr-num hidden-xs"}).text)
 
-
-
-        if latest_num != post_num :
+        if latest_num != post_num:
             latest_num = post_num
-            name =  posts.find("span", {"class" : "member"}).text
-            ago =  posts.find("div", {"class" : "wr-date hidden-xs"}).text
-            text = name + '의 새 글이 올라왔어욤' + '\n' +ago
+            name = posts.find("span", {"class": "member"}).text
+            ago = posts.find("div", {"class": "wr-date hidden-xs"}).text
+            text = name + '의 새 글이 올라왔어욤' + '\n' + ago
             bot.sendMessage(-1001555428405, text)
         time.sleep(30)
-        print('bot 동작 중 현재 게시글 번호' + latest_num)
+        print('bot 동작 중 현재 게시글 번호' + str(latest_num))
