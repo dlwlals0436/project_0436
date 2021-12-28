c40bda490190527c0e139c27b44ab55c5229a3db
dlwlals0436 <94441549+dlwlals0436@users.noreply.github.com>
2021-12-28 15:54:35 -0800

    Update toki0436.py

--- toki0436.py
+++ toki0436.py
@@ -12,11 +12,12 @@ import telegram
 
 bot = telegram.Bot(token='5047253923:AAGRE6Knv20XImmA_OO-WO5x8xW9dSGD2iU')
 
+
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
 
 if __name__ == '__main__':
 
-    
+    # 제일 최신 게시글의 번호 저장
     latest_num = 0
     while True:
         url = 'https://newtoki118.com/toki_free?sca=%EA%B3%B5%EC%9C%A0'
@@ -24,14 +25,13 @@ if __name__ == '__main__':
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
         posts = soup.find("li", {"class" : "list-item"})
-        post_num = int(posts.find("div", {"class" : "wr-num hidden-xs"}).text)
-
+        post_num = posts.find("div", {"class" : "wr-num.hidden-xs"})
 
 
         if latest_num != post_num :
             latest_num = post_num
             name =  posts.find("span", {"class" : "member"}).text
-            ago =  posts.find("div", {"class" : "wr-date hidden-xs"}).text
+            ago =  posts.find("div", {"class" : "wr-date.hidden-xs"}).text
             text = name + '의 새 글이 올라왔어욤' + '\n' +ago
             bot.sendMessage(-1001555428405, text)
         time.sleep(30)
