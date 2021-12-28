ae43d548a19b3ee1ebaeded97523675568700285
dlwlals0436 <94441549+dlwlals0436@users.noreply.github.com>
2021-12-27 22:19:37 -0800

    Update toki0436.py

--- toki0436.py
+++ toki0436.py
@@ -22,15 +22,15 @@ if __name__ == '__main__':
         req = requests.get('https://newtoki118.com/toki_free?sca=%EA%B3%B5%EC%9C%A0')
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
-        posts = soup.find("li", {"class" : "list-item"})
-        post_num = posts.find("div", {"class" : "wr-num hidden-xs"}).text
+        post = soup.find("li", {"class" : "list-item"})
+        post_num = post.find("div", {"class" : "wr-num hidden-xs"}).text
 
 
 
         if latest_num != post_num :
             latest_num = post_num
-            name =  posts.find("span", {"class" : "member"}).text
-            ago =  posts.find("div", {"class" : "wr-date hidden-xs"}).text
+            name =  post.find("span", {"class" : "member"}).text
+            ago =  post.find("div", {"class" : "wr-date hidden-xs"}).text
             text = name + '의 새 글이 올라왔어욤' + '\n' +ago
             bot.sendMessage(-1001555428405, text)
         time.sleep(30)
