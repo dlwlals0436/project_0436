6705597ffa1c659ca89b33306dd0f3f28d6a38f8
dlwlals0436 <94441549+dlwlals0436@users.noreply.github.com>
2021-12-28 20:33:02 -0800

    Update toki0436.py

--- toki0436.py
+++ toki0436.py
@@ -1,26 +1,24 @@
 
+import cloudscraper
 import requests
-
 import time
-
 from bs4 import BeautifulSoup
-
 import telegram
 
 
 bot = telegram.Bot(token='5047253923:AAGRE6Knv20XImmA_OO-WO5x8xW9dSGD2iU')
 
-
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
 cookies = {'cf_clearance': 'some clearance'}
+
+
 if __name__ == '__main__':
 
     # 제일 최신 게시글의 번호 저장
     latest_num = 0
     while True:
-        url = 'https://newtoki118.com/toki_free?sca=%EA%B3%B5%EC%9C%A0'
-        req = requests.get(url, headers = headers, cookies = cookies)
-        html = req.text
+        scraper = cloudscraper.create_scraper()
+        html = scraper.get("https://newtoki118.com/toki_free?sca=%EA%B3%B5%EC%9C%A0").content
         soup = BeautifulSoup(html, 'html.parser')
         posts = soup.find("li", {"class" : "list-item"})
         post_num = posts.find("div", {"class" : "wr-num hidden-xs"}).text
