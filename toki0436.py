02e131e83afa8a93a154493f3d4799010441eaca
dlwlals0436 <94441549+dlwlals0436@users.noreply.github.com>
2022-01-11 12:59:21 -0800

    Update toki0436.py

--- toki0436.py
+++ toki0436.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     latest_num = 0
     while True:
         scraper = cloudscraper.create_scraper()
-        html = scraper.get("https://newtoki118.com/toki_free?sca=%EA%B3%B5%EC%9C%A0").content
+        html = scraper.get("https://newtoki119.com/toki_free?sca=%EA%B3%B5%EC%9C%A0").content
         soup = BeautifulSoup(html, 'html.parser')
         posts = soup.find("li", {"class" : "list-item"})
         post_num = posts.find("div", {"class" : "wr-num hidden-xs"}).text
@@ -30,5 +30,5 @@ if __name__ == '__main__':
             ago =  posts.find("div", {"class" : "wr-date hidden-xs"}).text
             text = name + '의 새 글이 올라왔어욤' + "[" + ago + "]"
             bot.sendMessage(-1001555428405, text)
-        time.sleep(20)
+        time.sleep(15)
         print('bot 동작 중 현재 게시글 번호' + latest_num)
